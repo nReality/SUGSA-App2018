9b1330f5c96dfc299b3fa74e7b68317c4d7121e2
Hendri Pretorius <pretorh@gmail.com>
2017-08-16 15:41:25 +0200

    fix indentation

--- app/providers/tweet-share.ts
+++ app/providers/tweet-share.ts
@@ -1,35 +1,27 @@
-import {Injectable} from '@angular/core';
-import {Platform} from 'ionic-angular';
+import { Injectable } from '@angular/core';
+import { Platform }   from 'ionic-angular';
 
 @Injectable()
 export class TweetShare {
-
-
   constructor(private platform: Platform) {
-          this.platform = platform;
-        }
-
+    this.platform = platform;
+  }
 
   shareViaTwitter(message, image, link) {
     var pl = (<any>window).plugins
-    if (pl == null){
-       window.open(`https://twitter.com/intent/tweet?text=` + message);
+    if (pl == null) {
+      window.open(`https://twitter.com/intent/tweet?text=` + message);
       return;
     }
-      this.platform.ready().then(() => {
-          if(pl.socialsharing) {
-            try{
-              //for some reason the check always fails
-              //pl.socialsharing.canShareVia("twitter", message, null, image, link, function(result) {
-                  pl.socialsharing.shareViaTwitter(message, image, link);
-              //}, function(error) {
-              //    console.error(error);
-              //});
-            }catch(error){
-                console.error(error);
-            }
 
-          }
-      });
+    this.platform.ready().then(() => {
+      if (pl.socialsharing) {
+        try {
+          pl.socialsharing.shareViaTwitter(message, image, link);
+        } catch(error) {
+          console.error(error);
+        }
+      }
+    });
   }
 }
